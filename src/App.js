b82b52257536f19fb392c052e77076005eed14f3
Mercy-Kanyi <wakonyocasey@gmail.com>
2022-09-03 00:06:00 +0300

    removed unnecessary files and created react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Weatherrrrr</h1>
+      <footer>
+        <a href="www.shecodes.io">Open-source code</a> by Mercy Kanyi
+      </footer>
+    </div>
+  );
+}
+
+export default App;
